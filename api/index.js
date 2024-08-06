a4d3d38dee0e60ce1d03e943c6ab933f23ef61ec
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-08-06 18:47:56 +0545

    userModel and testapi

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -12,6 +14,13 @@ mongoose
     console.log(err);
   });
 const app = express();
+
 app.listen(3001, () => {
   console.log("Server is running on port:3001");
 });
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Api is working" });
+});
+
+app.use("/api/user", userRoutes);
