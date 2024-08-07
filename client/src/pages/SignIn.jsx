7780a03064c3f0c1868d44dd8603a7dd8ba3785e
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-08-07 17:15:36 +0545

    signUpPage

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
-}
+  return (
+    <div>
+      SignIn
+      <Link to="/sign-up" className="text-blue-500">
+        Sign Up
+      </Link>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
